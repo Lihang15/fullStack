c0c9088481cd9e95923dc7de391e19a754d6eb4d
wanglihang <wanglihang@iauto.com>
2021-01-25 18:30:02 +0800

    123

--- express-genteratr/express10/routes/file.js
+++ express-genteratr/express10/routes/file.js
@@ -1,14 +1,16 @@
 var express = require('express');
 var multer = require('multer')
-var path =require('path')
+var path = require('path')
 var silly = require('silly-datetime')
-var mkdirp =require('mkdirp');
-const { dirname } = require('path');
+var mkdirp = require('mkdirp');
+const {
+  dirname
+} = require('path');
 // var mu = multer({dest:'public/uploads'})
 var storage = multer.diskStorage({
   destination: async function (req, file, cb) {
-    let day = silly.format(new Date(),"YYYYMMDD")
-    let dirpath = path.join('public/uploads',day)
+    let day = silly.format(new Date(), "YYYYMMDD")
+    let dirpath = path.join('../public/uploads', day)
     await mkdirp(dirpath)
     cb(null, dirpath)
   },
@@ -17,17 +19,30 @@ var storage = multer.diskStorage({
     cb(null, file.originalname)
   }
 })
- 
-var upload = multer({ storage: storage })
+
+var upload = multer({
+  storage: storage
+})
 var router = express.Router();
 /* GET home page. */
-router.get('/form', function(req, res, next) {
-  res.render('index', { title: 'Express' });
-});
-router.post('/postFile', upload.single("pic"),function(req, res, next) {
-    console.log(req.body)
-    console.log(req.file)
-    res.send("post")
+router.get('/form', function (req, res, next) {
+  res.render('index', {
+    title: 'Express'
   });
+});
+//单文件上传
+router.post('/postFile', upload.single("pic"), function (req, res, next) {
+  console.log(req.body)
+  console.log(req.file)
+  console.log(req.files)
+  res.send("post")
+});
 
+//多文件上传
+// router.post('/postFile', upload.array("pic"), function (req, res, next) {
+//   console.log(req.body)
+//   console.log(req.file)
+//   console.log(req.files)
+//   res.send("post")
+// });
 module.exports = router;
