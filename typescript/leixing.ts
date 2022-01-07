2681feff1bfdedc0cb1b233346b0808576fbbfa0
wanglihang <lh.wang@rootant.com>
2022-01-07 16:38:09 +0800

    add never

--- typescript/leixing.ts
+++ typescript/leixing.ts
@@ -33,7 +33,21 @@ enum direction{
 console.log(em)
 
 
+// 返回never的函数总是返回的异常
+function error(message: string): never {
+  throw new Error(message);
+}
 
+// 推断的返回值类型为never
+function fail() {
+  return error("Something failed");
+}
+
+// 返回never的函数必须存在无法达到的终点
+function infiniteLoop(): never {
+  while (true) {
+  }
+}
 
 
 
