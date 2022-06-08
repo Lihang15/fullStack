acc05a58f804f758dfcf8ba8fff5dcc9542ce8f3
wanglihang <lh.wang@rootant.com>
2022-06-08 18:47:10 +0800

    fix:update

--- typescript/leixing.ts
+++ typescript/leixing.ts
@@ -99,3 +99,27 @@ type funcTypeOrString = string | funcType
 //   }
 
 // console.log(func(123456))
+
+
+
+interface User {
+  id: number;
+  age: number;
+  name: string;
+};
+
+// 相当于: type PartialUser = { id?: number; age?: number; name?: string; }
+type PartialUser = Partial<User>
+
+// 相当于: type PickUser = { id: number; age: number; }
+type PickUser = Pick<User, "id" | "age">
+
+// 相当于: type RequiredUser = { id: number; age: number;name: string }
+type RequiredUser = Required<User>
+
+// 相当于: type PickUser = { age: number; name: string; }
+type OmitUser = Omit<User, "id">
+
+// 相当于 可以自定义健值对
+const record:Record<string,string>={'123':'456',name:'123'}
+console.log(record)
