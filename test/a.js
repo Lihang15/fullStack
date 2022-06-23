30ab65221328877089bcd2df04752b08f9cef2c3
wanglihang <lh.wang@rootant.com>
2022-06-23 14:24:46 +0800

    fix: 修改文件

--- test/a.js
+++ test/a.js
@@ -1,23 +1,9 @@
 
 
-function person(arg1,arg2){
-    console.log(this.name)
-    console.log(this)
-    console.log(arg1,arg2)
- }
+console.log('123')
+let o = {
+   name:setInterval(()=>{console.log('一次时间循环')},2000)
+}
 
- var egg ={
-    name:'王老师'
- }
-
- Function.prototype.newBind =function(obj,...arg){
-    var that =this
-    return function(){
-        that.call(obj,...arg);
-    }
- }
- person.newCall(egg,'xx','bb')
-
-
-
-
+console.log('456')
+clearInterval(o.name)
