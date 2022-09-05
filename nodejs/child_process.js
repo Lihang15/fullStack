f52ef678d8eb3e932b2ff1ac7d6cba8300ce7823
wanglihang <lh.wang@rootant.com>
2022-09-05 18:56:52 +0800

    fix: 添加文件

--- nodejs/child_process.js
+++ nodejs/child_process.js
@@ -12,9 +12,9 @@ const childprocess = require('child_process');
 //     console.log('子进程退出码:',code)
 // })
 
+
+
 //spawn 和exec效果一样 传入参数不一样 stdout不在回调函数中
-// spawn 和 fork相似 都需要传入一个执行文件路径 fork可以父子进程之间通信
-// fork用法查看同级目录 多进程ipc通信
 // node ./test.js 参数
 const p2 = childprocess.spawn('node',['./test.js','参数'])
 p2.stdout.on('data',function(data){
@@ -27,3 +27,6 @@ p2.stderr.on('data', function (data) {
 p2.on('close', function (code) {
     console.log('子进程退出码 '+code);
  });
+
+
+// fork用法查看同级目录 多进程ipc通信 fork可以父子进程之间通信 需要传入一个nodejs文件（nodejs模块充当子进程）的目录
